c1a0dab7cbe24e5d14549276dc811daf294d4f32
Thor Wei Jie <thorweijie@yahoo.com>
2021-10-13 17:03:31 +0800

    Updated test port from 8080 to 5004

--- coqui_server/client.py
+++ coqui_server/client.py
@@ -1,11 +1,11 @@
 import sys
 import websocket
-    
-ws = websocket.WebSocket()
-ws.connect("ws://localhost:8080/api/v1/stt")
 
-with open(f"{sys.argv[1]}", mode='rb') as file:  # b is important -> binary
+ws = websocket.WebSocket()
+ws.connect("ws://localhost:5004/api/v1/stt")
+
+with open(f"{sys.argv[1]}", mode="rb") as file:  # b is important -> binary
     audio = file.read()
     ws.send_binary(audio)
-    result =  ws.recv()
-    print(result) # Print text transcription received from server
+    result = ws.recv()
+    print(result)  # Print text transcription received from server
